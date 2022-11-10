d45a4a1498c7f6c6262b0e7874ce9f535554322b
xindezhentu <xinndezhentu@atguigu.com>
2022-11-10 10:31:42 +0800

    main commit

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -8,6 +8,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println(1);
         System.out.println("hot-fix");
-
+        System.out.println("main");
     }
 }
